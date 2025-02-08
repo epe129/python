ed462e8849857bdadb3de942cd7435faf508e9c9
epe129 <lenniplays12@gmail.com>
2025-02-08 13:38:53 +0200

    asd

--- EnKeksiGategoriaa/bingolappu.py
+++ EnKeksiGategoriaa/bingolappu.py
@@ -1,11 +1,10 @@
 import random
 
-value = 10//2
-
-
-for x in range(4):
-    print(" ","_" * 21)
-    for i in range(4):
-        numbers = random.randint(1,99)
-        print(" ",f"{numbers:2}|", end=" ")
-print("___________________")
+print("-" * 16)
+for i in range(4):
+    for j in range(4):
+        numbers = random.randint(1, 99)
+        print(f"|{numbers:2}|", end="")
+    print("")  
+print("-" * 16, end='')  
+ 
