0b0be9244fa6363f1c5f8bf5d7ad2a3ff0d767a9
epe129 <eemelik985@gmail.com>
2025-03-11 21:11:33 +0200

    a

--- harjoituksia/sudokutarkistin.py
+++ harjoituksia/sudokutarkistin.py
@@ -45,7 +45,7 @@ def sarake_oikein(sudoku: list):
     if len(sarake) >= 9:
         sarake = sarake[:8]
     if sudoku[c][x] in sarake:
-        print(sarake)
+        # print(sarake)
         for k in sarake:
             # print(k)
             if 1 == k or 2 == k or 3 == k or 4 == k or 5 == k or 6 == k or 7 == k or 8 == k or 9 == k:
@@ -101,15 +101,15 @@ def nelio_oikein(sudoku: list):
 
 if __name__=="__main__":
     sudoku = [
-    [ 2, 6, 7, 8, 3, 9, 5, 0, 4 ],
-    [ 9, 0, 3, 5, 1, 0, 6, 0, 0 ],
-    [ 0, 5, 6, 0, 0, 0, 8, 3, 9 ],
-    [ 5, 1, 9, 0, 4, 6, 3, 2, 8 ],
-    [ 8, 0, 2, 1, 0, 5, 7, 0, 6 ],
-    [ 6, 7, 4, 3, 2, 0, 0, 0, 5 ],
-    [ 0, 0, 0, 4, 5, 7, 2, 6, 3 ],
-    [ 3, 2, 0, 0, 8, 0, 0, 5, 7 ],
-    [ 7, 4, 5, 0, 0, 3, 9, 0, 1 ],
+     [2, 6, 7, 8, 3, 9, 5, 0, 4],
+     [9, 0, 3, 5, 1, 0, 6, 0, 0],
+     [0, 5, 1, 6, 0, 0, 8, 3, 9],
+     [5, 1, 9, 0, 4, 6, 3, 2, 8],
+     [8, 0, 2, 1, 0, 5, 7, 0, 6],
+     [6, 7, 4, 3, 2, 0, 0, 0, 5],
+     [0, 0, 0, 4, 5, 7, 2, 6, 3],
+     [3, 2, 0, 0, 8, 0, 0, 5, 7],
+     [7, 4, 5, 0, 0, 3, 9, 0, 1]
   ]
     print(rivi_oikein(sudoku))
     print(sarake_oikein(sudoku))
