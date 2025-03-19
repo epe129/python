8ae661eff671a85a94e9392e070fad92cdc26c36
epe129 <eemelik985@gmail.com>
2025-03-19 19:33:18 +0200

    asd

--- harjoituksia/har.py
+++ harjoituksia/har.py
@@ -187,3 +187,102 @@ for i in suoritukset:
         pass
 
 print(nähäty)
+
+
+def lisaa_opiskelija(opiskelijat: dict, nimi: str):
+    opiskelijat[nimi] = set()
+
+def lisaa_suoritus(opiskelijat: dict, nimi: str, suoritus: tuple):
+    if suoritus[1] == 0:
+        return 0           
+    opiskelijat[nimi].add(suoritus)
+        
+def tulosta(opiskelijat: dict, nimi: str):
+    if nimi not in opiskelijat:
+        print(f"ei löytynyt ketään nimellä {nimi}") 
+    for avain, arvot in opiskelijat.items():
+        if avain == nimi:
+            print(f"{avain}:")
+            if arvot == "":
+                print(" ei suorituksia")
+            else:
+                nähdytArvot = {}
+                keskiarvo = 0
+                lukuja = 0
+                
+                for avain2,arvot2 in arvot:
+                    nähdytArvot[avain2] = arvot2
+                suoritukset = len(nähdytArvot)
+                for a,s in nähdytArvot.items():
+                    keskiarvo += s
+                    lukuja += 1
+
+                if suoritukset == 0:
+                    print(" ei suorituksia")
+                else:
+                    print(f" suorituksia {suoritukset} kurssilta:") 
+                
+                for a,s in nähdytArvot.items():
+                    print(f"  {a} {s}")
+                
+                if keskiarvo == 0:
+                    pass
+                else:
+                    print(f" keskiarvo {keskiarvo/lukuja}")
+                
+   
+
+def kooste(opiskelijat: dict):
+    print(f"opiskelijoita {len(opiskelijat)}")
+    edellinen = 0
+    keskiarvot = {}
+    summa = 0
+    e = 0
+    for avaint,arvot in opiskelijat.items():
+        if len(arvot) > edellinen:
+            edellinen = len(arvot)
+        for nimi,numero in arvot:
+            summa += numero
+        keskiarvot[avaint] = summa
+        summa = 0
+    for w,i in keskiarvot.items():
+        if i > e:
+            e = i
+    print(f"eniten suorituksia",len(arvot),avaint)
+    print("paras keskiarvo",i/len(arvot) ,w)
+        
+
+if __name__=="__main__":
+    opiskelijat = {}
+    lisaa_opiskelija(opiskelijat, "emilia")
+    lisaa_opiskelija(opiskelijat, "pekka")
+    lisaa_suoritus(opiskelijat, "emilia", ("ohpe", 5))
+    lisaa_suoritus(opiskelijat, "emilia", ("tikape", 4))
+    lisaa_suoritus(opiskelijat, "pekka", ("tira", 3))
+    tulosta(opiskelijat, "emilia")
+    # print(opiskelijat)
+
+
+
+# nähäty = {}
+    # for i in suoritukset:
+    #     aine, numero = i
+    #     if aine not in nähäty and numero > 0:
+    #         nähäty[aine] = numero
+    #     if aine in nähäty:
+    #         edlli=numero
+    #         e = aine
+    #         for avain,numero in nähäty.items():
+    #             if numero > edlli and avain == e:
+    #                 nähäty[aine] = numero                
+    #             if numero < edlli and avain == e:
+    #                 nähäty[aine] = edlli
+    #     else:
+    #         pass
+    # print(nähäty)
+    # for i in suoritukset:
+    #     aine, numero = i
+    #     if numero == 0:
+    #         del i
+    #     else:
+    #         opiskelijat[nimi].append(i)
