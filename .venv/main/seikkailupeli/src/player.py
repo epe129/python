9dd7311c981f3d6b1b57e26aaecdd2984c787975
epe129 <eemelik985@gmail.com>
2025-08-22 13:24:22 +0300

    commit

--- .venv/main/seikkailupeli/src/player.py
+++
@@ -1,8 +0,0 @@
-class Player:
-    def __init__(self, current_room):
-        self.current_room = current_room
-        self.inventory = []
-        self.score = 0  # Pelaajan pisteet
-
-    def add_score(self, points):
-        self.score += points
--- .venv/main/seikkailupeli/src/utils.py
+++
@@ -1,7 +0,0 @@
-def load_image(path, size=None):
-    """Lataa kuvan ja skaalaa sen tarvittaessa."""
-    import pygame
-    image = pygame.image.load(path)
-    if size:
-        image = pygame.transform.scale(image, size)
-    return image
